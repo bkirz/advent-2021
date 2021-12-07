f034098e9074bc32172ba9aa8d4f96f4164f8b66
Ben Kirzhner <ben.kirzhner@gmail.com>
2021-12-07 03:21:16 -0800

    Cleanup day 6

--- day_6.ts
+++ day_6.ts
@@ -5,16 +5,19 @@ const NEW_FISH_TIME = 8;
 
 function advanceTimers(timers: timerCounts): timerCounts {
     const newTimers: timerCounts = new Map();
+    // For all fish with timer N > 0, tick their timers down 1.
     for (let age = 1; age <= NEW_FISH_TIME; age++) {
         newTimers.set(age - 1, timers.get(age) || 0);
     }
+    // For fish with timers set to 0, spawn a new fish for each one with timer = 8...
     newTimers.set(8, timers.get(0) || 0);
+    // ...and add their timers to the pool of timers to be set to 6.
     newTimers.set(6, (timers.get(7) || 0) + (timers.get(0) || 0));
     return newTimers;
 }
 
-function runForDays(startTimers: timerCounts, days: number): timerCounts {
-    return range(1, days).reduce((timers, day) => { return advanceTimers(timers); }, startTimers);
+function runForDays(initialTimers: timerCounts, days: number): timerCounts {
+    return range(1, days).reduce((timers, _day) => advanceTimers(timers), initialTimers);
 }
 
 let timers = tally(loadInput('day_6.input').split(',').map((n) => parseInt(n)));

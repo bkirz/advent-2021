4fb3f527270be0f5147d5fb8851f30c8368f4015
Ben Kirzhner <ben.kirzhner@gmail.com>
2021-12-15 01:04:58 -0800

    Day 15 part 2

--- day_15.ts
+++ day_15.ts
@@ -18,38 +18,71 @@ function neighbors({row, col}: Point, grid: Grid) {
     ].filter(({row, col}) => row >= 0 && row < colLength && col >= 0 && col < rowLength);
 }
 
-// We can envision the cave as a directed graph where every cell is a node with edges
-// leaving towards and entering from its neighbors. Edges are weighted using the destination node.
-// From there, we just use dijkstra's.
+function findLowestTotalRisk(grid: Grid): number {
+    // We can envision the cave as a directed graph where every cell is a node with edges
+    // leaving towards and entering from its neighbors. Edges are weighted using the destination node.
+    // From there, we just use dijkstra's.
+
+    const destination = {row: grid.length - 1, col: grid[grid.length - 1].length - 1};
+
+    const distances = grid.map(row => new Array(row.length).fill(Infinity));
+    const visited = grid.map(row => new Array(row.length).fill(false));
+
+    const queue: Point[] = [];
+
+    distances[START.row][START.col] = 0;
+
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[row].length; col++) {
+            queue.push({row, col});
+        }
+    }
+
+    while (queue.length > 0) {
+        const currentNode = minBy(queue, ({row, col}) => distances[row][col]);
+        queue.splice(queue.indexOf(currentNode), 1);
+
+        for (const neighbor of neighbors(currentNode, grid)) {
+            const newDistance = distances[currentNode.row][currentNode.col] + grid[neighbor.row][neighbor.col];
+            if (newDistance < distances[neighbor.row][neighbor.col]) {
+                distances[neighbor.row][neighbor.col] = newDistance;
+            }
+        }
+    }
+
+    return distances[destination.row][destination.col];
+}
+
+function tessellateGrid(grid: Grid, repetitions: number): Grid {
+    const newGrid = [];
+
+    // The row of the cell in the original grid to be transformed and applied
+    for (let originalRow = 0; originalRow < grid.length; originalRow++) {
+        // The col of the cell in the original grid to be transformed and applied
+        for (let originalCol = 0; originalCol < grid[originalRow].length; originalCol++) {
+            const originalValue = grid[originalRow][originalCol];
+
+            // The row corresponding to the repetition of the original grid on the new one
+            for (let metaRow = 0; metaRow < repetitions; metaRow++) {
+                // The col corresponding to the repetition of the original grid on the new one
+                for (let metaCol = 0; metaCol < repetitions; metaCol++) {
+                    const newValue = ((originalValue - 1 + metaRow + metaCol) % 9) + 1;
+                    const targetRow = originalRow + metaRow * grid.length;
+                    const targetCol = originalCol + metaCol * grid[originalRow].length;
+
+                    if (newGrid[targetRow] === undefined) { newGrid[targetRow] = []};
+
+                    newGrid[targetRow][targetCol] = newValue;
+                }
+            }
+        }
+    }
+
+    return newGrid;
+}
 
 const input = loadInput('day_15.input');
 const grid = input.split("\n").map(line => line.split('').map(cell => parseInt(cell)));
 
-const destination = {row: grid.length - 1, col: grid[grid.length - 1].length - 1};
-
-const distances = grid.map(row => new Array(row.length).fill(Infinity));
-const visited = grid.map(row => new Array(row.length).fill(false));
-
-const queue: Point[] = [];
-
-distances[START.row][START.col] = 0;
-
-for (let row = 0; row < grid.length; row++) {
-    for (let col = 0; col < grid[row].length; col++) {
-        queue.push({row, col});
-    }
-}
-
-while (queue.length > 0) {
-    const currentNode = minBy(queue, ({row, col}) => distances[row][col]);
-    queue.splice(queue.indexOf(currentNode), 1);
-
-    for (const neighbor of neighbors(currentNode, grid)) {
-        const newDistance = distances[currentNode.row][currentNode.col] + grid[neighbor.row][neighbor.col];
-        if (newDistance < distances[neighbor.row][neighbor.col]) {
-            distances[neighbor.row][neighbor.col] = newDistance;
-        }
-    }
-}
-
-console.log("Part 1", distances[destination.row][destination.col]);
+console.log("Part 1", findLowestTotalRisk(grid));
+console.log("Part 2", findLowestTotalRisk(tessellateGrid(grid, 5)));

aa4b8dca877bec901696f24cea2fc87c6b618ab0
Ben Kirzhner <ben.kirzhner@gmail.com>
2021-12-31 18:45:17 -0800

    a bit of day 21 refactoring in prep for part 2

--- day_21.ts
+++ day_21.ts
@@ -1,27 +1,48 @@
-import { loadInput } from "./helpers";
-
-type Position = number;
-type PlayerState = { trackPosition: Position, pointTotal: number };
-type Player = 'p1' | 'p2';
-type GameState = {
-    p1: PlayerState,
-    p2: PlayerState,
-    nextPlayer: Player,
-    totalDieRolls: number,
-    lastNumberRolled: number,
-};
+import { loadInput, range } from "./helpers";
 
 const TRACK_LENGTH = 10;
-const DIE_SIZE = 100;
-const POINT_THRESHOLD = 1000;
+const DIE_ROLLS_PER_TURN = 3;
 const INPUT_PATTERN = /Player \d starting position: (\d)/;
+
+type Position = number;
+interface PlayerState { readonly trackPosition: Position; readonly pointTotal: number; }
+type Player = 'p1' | 'p2';
+interface DieRoll { readonly die: Die; readonly result: number; };
+interface Die { readonly lastNumberRolled: number; readonly roll: () => DieRoll; }
+interface GameState {
+    readonly p1: PlayerState;
+    readonly p2: PlayerState;
+    readonly nextPlayer: Player;
+    readonly totalDieRolls: number;
+    readonly die: Die;
+};
+
+class DeterministicDie implements Die {
+    static SIZE: number = 100;
+    lastNumberRolled: number;
+
+    constructor(lastNumberRolled: number = DeterministicDie.SIZE) {
+        this.lastNumberRolled = lastNumberRolled;
+    }
+
+    roll(): DieRoll {
+        const result = (this.lastNumberRolled % DeterministicDie.SIZE) + 1;
+        return { die: new DeterministicDie(result), result };
+    }
+}
 
 function stepGameState(state: GameState): GameState {
     const [activePlayer, newNextPlayer]: [Player, Player] =
         state.nextPlayer === 'p1' ? ['p1', 'p2'] : ['p2', 'p1'];
 
-    const nextDieRolls = [1, 2, 3].map(n => ((state.lastNumberRolled + n - 1) % DIE_SIZE) + 1);
-    const stepsToTake = nextDieRolls.reduce((a, b) => a + b);
+    const nextDieRolls: DieRoll[] = [];
+    let die = state.die;
+    for (let rollNum = 0; rollNum < DIE_ROLLS_PER_TURN; rollNum++) {
+        const roll = die.roll();
+        die = roll.die;
+        nextDieRolls.push(roll);
+    }
+    const stepsToTake = nextDieRolls.map(roll => roll.result).reduce((a, b) => a + b, 0);
     const newPosition = (state[activePlayer].trackPosition - 1 + stepsToTake) % TRACK_LENGTH + 1;
     const newPointTotal = state[activePlayer].pointTotal + newPosition;
 
@@ -30,14 +51,14 @@ function stepGameState(state: GameState): GameState {
         [activePlayer]: {trackPosition: newPosition, pointTotal: newPointTotal},
         nextPlayer: newNextPlayer,
         totalDieRolls: state.totalDieRolls + nextDieRolls.length,
-        lastNumberRolled: nextDieRolls[nextDieRolls.length - 1],
+        die: die,
     }
 }
 
-function advanceStateUntilVictory(state: GameState): GameState {
+function advanceStateUntilVictory(state: GameState, pointThreshold: number): GameState {
     let gameState = initialGameState;
 
-    while (gameState.p1.pointTotal < POINT_THRESHOLD && gameState.p2.pointTotal < POINT_THRESHOLD) {
+    while (gameState.p1.pointTotal < pointThreshold && gameState.p2.pointTotal < pointThreshold) {
         gameState = stepGameState(gameState);
     }
 
@@ -54,12 +75,12 @@ const initialGameState: GameState = {
     p1: { trackPosition: p1Start, pointTotal: 0 },
     p2: { trackPosition: p2Start, pointTotal: 0 },
     totalDieRolls: 0,
-    lastNumberRolled: DIE_SIZE,
     nextPlayer: 'p1',
+    die: new DeterministicDie(),
 };
 
 // The winner must have been the one who just played, so the next player is the loser.
-const finalState = advanceStateUntilVictory(initialGameState);
+const finalState = advanceStateUntilVictory(initialGameState, 1000);
 const losingPlayer = finalState.nextPlayer;
 
 console.log("Part 1", finalState[losingPlayer].pointTotal * finalState.totalDieRolls);

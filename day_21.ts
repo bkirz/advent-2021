c2b783c263140c505c1998252d8bc6a683086036
Ben Kirzhner <ben.kirzhner@gmail.com>
2022-01-02 14:18:35 -0800

    Day 21 part 2

--- day_21.ts
+++ day_21.ts
@@ -1,4 +1,5 @@
-import { loadInput, range } from "./helpers";
+import assert from "assert";
+import { ImmutableHashMap, ImmutableHashSet, loadInput, tallyBy } from "./helpers";
 
 const TRACK_LENGTH = 10;
 const DIE_ROLLS_PER_TURN = 3;
@@ -7,8 +8,9 @@ const INPUT_PATTERN = /Player \d starting position: (\d)/;
 type Position = number;
 interface PlayerState { readonly trackPosition: Position; readonly pointTotal: number; }
 type Player = 'p1' | 'p2';
+const PLAYERS: Player[] = ['p1', 'p2'];
 interface DieRoll { readonly die: Die; readonly result: number; };
-interface Die { readonly lastNumberRolled: number; readonly roll: () => DieRoll; }
+interface Die { readonly lastNumberRolled: number; readonly roll: () => DieRoll[]; }
 interface GameState {
     readonly p1: PlayerState;
     readonly p2: PlayerState;
@@ -25,44 +27,125 @@ class DeterministicDie implements Die {
         this.lastNumberRolled = lastNumberRolled;
     }
 
-    roll(): DieRoll {
+    roll(): DieRoll[] {
         const result = (this.lastNumberRolled % DeterministicDie.SIZE) + 1;
-        return { die: new DeterministicDie(result), result };
+        return [{ die: new DeterministicDie(result), result }];
     }
 }
 
-function stepGameState(state: GameState): GameState {
+class DiracDie implements Die {
+    // not supported
+    lastNumberRolled: number = 0;
+
+    roll(): DieRoll[] {
+        return [1, 2, 3].map(result => ({ die: this, result }));
+    }
+}
+
+function stepGameState(state: GameState): GameState[] {
     const [activePlayer, newNextPlayer]: [Player, Player] =
         state.nextPlayer === 'p1' ? ['p1', 'p2'] : ['p2', 'p1'];
 
-    const nextDieRolls: DieRoll[] = [];
-    let die = state.die;
+    let nextDieRolls: DieRoll[][] = [[]];
     for (let rollNum = 0; rollNum < DIE_ROLLS_PER_TURN; rollNum++) {
-        const roll = die.roll();
-        die = roll.die;
-        nextDieRolls.push(roll);
-    }
-    const stepsToTake = nextDieRolls.map(roll => roll.result).reduce((a, b) => a + b, 0);
-    const newPosition = (state[activePlayer].trackPosition - 1 + stepsToTake) % TRACK_LENGTH + 1;
-    const newPointTotal = state[activePlayer].pointTotal + newPosition;
+        const newRollHistories = [];
+        for (const rollHistory of nextDieRolls) {
+            const previousRollResult = rollHistory[rollHistory.length - 1];
+            const die = previousRollResult === undefined ? state.die : previousRollResult.die;
+            const newRolls = die.roll();
 
-    return {
-        ...state,
-        [activePlayer]: {trackPosition: newPosition, pointTotal: newPointTotal},
-        nextPlayer: newNextPlayer,
-        totalDieRolls: state.totalDieRolls + nextDieRolls.length,
-        die: die,
+            for (const newRoll of newRolls) {
+                newRollHistories.push(rollHistory.concat(newRoll));
+            }
+        }
+        nextDieRolls = newRollHistories;
     }
+
+    return nextDieRolls.map((rollHistory: DieRoll[]) => {
+        const stepsToTake = rollHistory.map(roll => roll.result).reduce((a, b) => a + b, 0);
+        const newPosition = (state[activePlayer].trackPosition - 1 + stepsToTake) % TRACK_LENGTH + 1;
+        const newPointTotal = state[activePlayer].pointTotal + newPosition;
+
+        const lastRoll = rollHistory[rollHistory.length - 1];
+
+        return {
+            ...state,
+            [activePlayer]: {trackPosition: newPosition, pointTotal: newPointTotal},
+            nextPlayer: newNextPlayer,
+            totalDieRolls: state.totalDieRolls + rollHistory.length,
+            die: lastRoll.die,
+        };
+    });
 }
 
 function advanceStateUntilVictory(state: GameState, pointThreshold: number): GameState {
-    let gameState = initialGameState;
+    let gameState = state;
 
     while (gameState.p1.pointTotal < pointThreshold && gameState.p2.pointTotal < pointThreshold) {
-        gameState = stepGameState(gameState);
+        const newGameStates = stepGameState(gameState);
+        assert.equal(newGameStates.length, 1);
+        gameState = newGameStates[0];
     }
 
     return gameState;
+}
+
+function countVictoriesByPlayer(initialState: GameState, pointThreshold: number): {p1: number, p2: number} {
+    function isTerminalState(state: GameState): boolean {
+        return PLAYERS.some(p => state[p].pointTotal >= pointThreshold);
+    }
+
+    function hashState(state: GameState): string {
+        const {p1, p2} = state;
+        return `[(${state.nextPlayer})p1:${p1.pointTotal}@${p1.trackPosition},p2:${p2.pointTotal}@${p2.trackPosition}]`;
+    }
+
+    function mergeCountMaps<K>(a: ImmutableHashMap<K, number>, b: ImmutableHashMap<K, number>): ImmutableHashMap<K, number> {
+        return a.merge(b, (_key, aCount, bCount) => aCount + bCount);
+    }
+
+    // Memoize using a map of unfinished game states to number of outcomes.
+    //
+    // Track completed states separately as a map of winner to count to avoid tracking individual end states.
+    // State to use when hashing for memoization:
+    //   - Player positions and point totals
+    //   - next player
+    // Because there are:
+    //   - ten spaces per player
+    //   - two possible active players
+    //   - 21 possible point values for each player
+    // This leaves 88.2K possible game states, which is a sufficiently small number for memoization.
+
+    let outcomes = new ImmutableHashMap<GameState, number>(hashState, [[initialState, 1]]);
+    let wins = {p1: 0, p2: 0};
+
+    while (outcomes.size() > 0) {
+        console.log("stepped game states so far", [...outcomes.entries()].map(([state, count]) => [hashState(state), count]));
+        console.log("wins so far", wins);
+
+        const newOutcomesMaps = [...outcomes.entries()].map(([state, count]) => {
+            const newStates: GameState[] = stepGameState(state);
+            const counts: ImmutableHashMap<GameState, number> =
+                tallyBy(newStates, hashState).mapValues(c => c * count);
+            return counts;
+        })
+
+        // This is slow as hell. It works, but would be much faster by merging them all at once.
+        const newOutcomes = newOutcomesMaps.reduce(mergeCountMaps);
+
+        const [terminalStates, inProgressStates] =
+            newOutcomes.partition((state, _count) => isTerminalState(state));
+
+        outcomes = inProgressStates;
+
+        const [p2Victories, p1Victories] =
+            terminalStates.partition((state, _count) => state.nextPlayer == "p1");
+
+        wins.p1 += [...p1Victories.values()].reduce((a, b) => a + b, 0);
+        wins.p2 += [...p2Victories.values()].reduce((a, b) => a + b, 0);
+    }
+
+    return wins;
 }
 
 const input = loadInput('day_21.input');
@@ -71,7 +154,7 @@ const [p1Start, p2Start] = input.split("\n").map(line => {
     return parseInt(position);
 });
 
-const initialGameState: GameState = {
+const part1InitialGameState: GameState = {
     p1: { trackPosition: p1Start, pointTotal: 0 },
     p2: { trackPosition: p2Start, pointTotal: 0 },
     totalDieRolls: 0,
@@ -80,7 +163,11 @@ const initialGameState: GameState = {
 };
 
 // The winner must have been the one who just played, so the next player is the loser.
-const finalState = advanceStateUntilVictory(initialGameState, 1000);
+const finalState = advanceStateUntilVictory(part1InitialGameState, 1000);
 const losingPlayer = finalState.nextPlayer;
 
 console.log("Part 1", finalState[losingPlayer].pointTotal * finalState.totalDieRolls);
+
+const part2InitialGameState: GameState = { ...part1InitialGameState, die: new DiracDie() };
+const victoriesByPlayer = countVictoriesByPlayer(part2InitialGameState, 21);
+console.log("Part 2", Math.max(victoriesByPlayer.p1, victoriesByPlayer.p2));
--- helpers.ts
+++ helpers.ts
@@ -6,7 +6,7 @@ export const loadInput =
     return fs.readFileSync(path.resolve(__dirname, filename)).toString();
   };
 
-// Generates an Array containing numbers from start to end.
+// Generates an Array containing numbers from start to end, inclusive.
 export const range =
   (start: number, end: number): number[] => {
     const result = [];
@@ -61,9 +61,9 @@ export const partition =
     const base: [T[], T[]] = [[], []];
     return arr.reduce(([trueVals, falseVals], val) => {
       if (predicate(val)) {
-        return [trueVals.concat(val), falseVals];
+        return [[...trueVals, val], falseVals];
       } else {
-        return [trueVals, falseVals.concat(val)]
+        return [trueVals, [...falseVals, val]];
       }
     }, base)
   }
@@ -109,6 +109,20 @@ export function tally<T>(items: T[]): Map<T, number> {
     return map;
   }
 
+export function tallyBy<T>(items: Iterable<T>, hashFn: (elem: T) => string): ImmutableHashMap<T, number> {
+    const countsByHash = new Map<string, number>();
+    for (const elem of items) {
+      const hash = hashFn(elem);
+      if (countsByHash.has(hash)) {
+        countsByHash.set(hash, countsByHash.get(hash) + 1);
+      } else {
+        countsByHash.set(hash, 1);
+      }
+    }
+
+    return new ImmutableHashMap<T, number>(hashFn, [...items].map(elem => [elem, countsByHash.get(hashFn(elem))]));
+}
+
 export function minBy<T, U>(items: T[], transform: (elem: T) => U): T | undefined {
   if (items.length === 0) { return undefined; }
   return items.reduce((a, b) => transform(a) < transform(b) ? a : b);
@@ -117,4 +131,142 @@ export function minBy<T, U>(items: T[], transform: (elem: T) => U): T | undefine
 export function maxBy<T, U>(items: T[], transform: (elem: T) => U): T | undefined {
   if (items.length === 0) { return undefined; }
   return items.reduce((a, b) => transform(a) > transform(b) ? a : b);
+}
+
+export class ImmutableHashSet<T> implements Iterable<T> {
+  // Because Javascript Sets and Maps don't support custom object hashing for
+  // builtin maps and sets, we can implement a Set via map of Hash(T) => T.
+  private _map: Map<string, T>;
+  hashFn: (elem: T) => string;
+
+  constructor(hashFn: (elem: T) => string, elems?: Iterable<T>) {
+      this.hashFn = hashFn;
+      if (elems === undefined) {
+          this._map = new Map();
+      } else {
+          this._map = new Map([...elems].map(e => [hashFn(e), e]));
+      }
+  }
+
+  add(elem: T): ImmutableHashSet<T> {
+      return this.newWithSameHash([...this, elem]);
+  }
+
+  merge(elems: Iterable<T>): ImmutableHashSet<T> {
+      return this.newWithSameHash([...this, ...elems]);
+  }
+
+  subtract(toSubtract: ImmutableHashSet<T>): ImmutableHashSet<T> {
+      return this.filter(e => !toSubtract.has(e));
+  }
+
+  delete(elem: T): ImmutableHashSet<T> {
+      const newElems = [...this._map.entries()].filter(([key, val]) => key !== this.hashFn(elem)).map(([_, val]) => val);
+      return this.newWithSameHash(newElems);
+  }
+
+  filter(predicate: (elem: T) => boolean): ImmutableHashSet<T> {
+      return this.newWithSameHash([...this._map.values()].filter(predicate));
+  }
+
+  partition(predicate: (elem: T) => boolean): [ImmutableHashSet<T>, ImmutableHashSet<T>] {
+      const trueSet = [];
+      const falseSet = [];
+      for (const val of this._map.values()) {
+          predicate(val) ? trueSet.push(val) : falseSet.push(val);
+      }
+      return [
+          this.newWithSameHash(trueSet),
+          this.newWithSameHash(falseSet),
+      ];
+  }
+
+  map(transform: (elem: T) => T): ImmutableHashSet<T> {
+    return this.newWithSameHash([...this._map.values()].map(transform));
+  }
+
+  flatMap(transform: (elem: T) => Iterable<T>): ImmutableHashSet<T> {
+      return this.newWithSameHash([...this._map.values()].flatMap(val => [...transform(val)]));
+  }
+
+  has(elem: T): boolean { return this._map.has(this.hashFn(elem)); }
+  size(): number { return this._map.size; }
+
+  toString(): string { return `ImmutableHashSet(${[...this._map.values()]})`; }
+  [Symbol.iterator](): Iterator<T, any, undefined> { return this._map.values(); }
+
+  private newWithSameHash(elems: Iterable<T>): ImmutableHashSet<T> {
+      return new ImmutableHashSet(this.hashFn, elems);
+  }
+}
+
+export class ImmutableHashMap<K, V> implements Iterable<[K, V]> {
+  private readonly _map: Map<string, [K, V]>;
+  private readonly hashFn: (key: K) => string;
+
+  constructor(hashFn: (key: K) => string, initialElements: Iterable<[K, V]> = []) {
+    this.hashFn = hashFn;
+    this._map = new Map<string, [K, V]>([...initialElements].map(([k, v]) => [hashFn(k), [k, v]]));
+  }
+
+  size(): number { return this._map.size; }
+
+  // TODO: This should really return a map of different types, but that messes up the underlying
+  // hash function unless a new one is provided. Accepting a hash separately from the type sucks. :(
+  flatMap(transform: (k: K, v: V) => Iterable<[K, V]>): ImmutableHashMap<K, V> {
+    const newEntries = [...this._map].flatMap(([hash, [k, v]]: [string, [K, V]]) => [...transform(k, v)]);
+    return new ImmutableHashMap(this.hashFn, newEntries);
+  }
+
+  mapEntries(transform: (k: K, v: V) => [K, V]): ImmutableHashMap<K, V> {
+    const newEntries = [...this._map].map(([_, [k, v]]): [K, V] => transform(k, v));
+    return new ImmutableHashMap(this.hashFn, newEntries);
+  }
+
+  mapValues(transform: (v: V) => V): ImmutableHashMap<K, V> {
+    const newEntries = [...this._map].map(([_, [k, v]]): [K, V] => [k, transform(v)]);
+    return new ImmutableHashMap(this.hashFn, newEntries);
+  }
+
+  get(key: K): V { return this._map.get(this.hashFn(key))[1]; }
+  has(key: K): boolean { return this._map.has(this.hashFn(key)); }
+
+  set(key: K, value: V): ImmutableHashMap<K, V> {
+    return new ImmutableHashMap<K, V>(this.hashFn, [...this._map.values(), [key, value]]);
+  }
+
+  update(key: K, valueIfMissing: V, transform: (val: V) => V): ImmutableHashMap<K, V> {
+    if (this.has(key)) {
+      return this.set(key, transform(this.get(key)));
+    } else {
+      return this.set(key, valueIfMissing);
+    }
+  }
+
+  merge(other: Iterable<[K, V]>, onCollision: (key: K, my: V, their: V) => V): ImmutableHashMap<K, V> {
+    let newMap: Map<string, [K, V]> = new Map(this._map);
+    for (const [key, val] of other) {
+      const hash = this.hashFn(key);
+      if (newMap.has(hash)) {
+        newMap.set(hash, [key, onCollision(key, newMap.get(hash)[1], val)]);
+      } else {
+        newMap.set(hash, [key, val]);
+      }
+    }
+    return new ImmutableHashMap<K, V>(this.hashFn, newMap.values());
+  }
+
+  filter(predicate: (key: K, value: V) => boolean): ImmutableHashMap<K, V> {
+    return new ImmutableHashMap(this.hashFn, [...this._map.values()].filter(([k, v]) => predicate(k, v)));
+  }
+
+  partition(predicate: (key: K, value: V) => boolean): [ImmutableHashMap<K, V>, ImmutableHashMap<K, V>] {
+    const [trueEntries, falseEntries] = partition([...this._map.values()], ([key, val]) => predicate(key, val));
+    return [new ImmutableHashMap(this.hashFn, trueEntries), new ImmutableHashMap(this.hashFn, falseEntries)];
+  }
+
+  entries(): Iterable<[K, V]> { return this._map.values(); }
+  keys(): Iterable<K> { return [...this.entries()].map(([key, _]) => key); }
+  values(): Iterable<V> { return [...this.entries()].map(([_, val]) => val); }
+  [Symbol.iterator](): Iterator<[K, V], any, undefined> { return this.entries()[Symbol.iterator](); }
 }
